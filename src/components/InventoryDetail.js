39e8165d66ca749ffd1ac4cf0cb0cb61ee420419
Mike Wilkes <mikewilkes@outlook.com>
2023-06-14 16:42:02 -0700

    wip

--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 function InventoryDetail(props) {
   const { product, onBackToList, onDeleteProduct} = props;
   const { inventory, changeInventory } = props;
