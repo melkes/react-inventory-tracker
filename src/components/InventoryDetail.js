855f8a21cb3512d79ef9932ec65e5989441b51c4
Mike Wilkes <mikewilkes@outlook.com>
2023-06-14 18:50:14 -0700

    Removed unused hook instance

--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -1,10 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import useInventory from "./useInventory";
 
 function InventoryDetail(props) {
   const { product, onBackToList, onDeleteProduct} = props;
-  const {inventory, changeInventory} = useInventory();
   return (
     <React.Fragment>
       <hr />
